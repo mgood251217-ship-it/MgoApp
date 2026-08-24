cff8c80b72d5a5f8a23390039a89adc0b40cb7ef
Viki <mgood251217@gmail.com>
2026-08-24 11:47:59 +0700

    refactor : action in routes

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export async function testConnection() {
-	const { data } = await api.get("/test.php");
+	const { data } = await api.get("", { params: { action: "test_connection" } });
 	return data;
 }
