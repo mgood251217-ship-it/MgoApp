c8c299e70760509ab7cc04ccbe777b965f5c73d9
Viki <mgood251217@gmail.com>
2026-09-01 14:12:54 +0700

    feat : update versi

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const config = {
     appName: "MGO Desktop",
-    version: "1.0.8",
+    version: "2.0.0",
     // serverUrl: "http://localhost:5000",
     // serverUrl: "http://localhost/MgoAll/admin",
     serverUrl: "https://mgood.my.id/admin",
